209a074107f3e50427a22918428d72254f78398f
Masayuki Higashino <msyk@hgsn.info>
2017-02-28 12:37:35 +0900

    support hashids

--- lib/fishing-cat/client/cli.rb
+++ lib/fishing-cat/client/cli.rb
@@ -10,10 +10,25 @@ module FishingCat
       def test
         config = YAML.load_file('tmp/config.yml')
         FishingCat::Client.setup(config:config)
-        message = FishingCat::Client::Mailer.create(cid:cid, pid:pid, config:config)
-        puts message.body
-        # message.deliver_now
+        mails.each do |to|
+          pid = Hashids.new(config['salt'], 0, 'abcdefghijklmnopqrstuvwxyz').encode(SecureRandom.random_number(2**64))
+          deliver(to:to, cid:cid, pid:pid, config:config)
+        end
       end
+
+      no_commands do
+        def deliver(cid:, pid:, to:, config:)
+          message = FishingCat::Client::Mailer.create(cid:cid, pid:pid, config:config.merge({
+            'header' => {
+              'to' => to
+            }
+          }))
+          # message.deliver_now
+          $stdout.puts "#{cid}\t#{pid}\t#{to}"
+          # $stderr.puts message.body
+        end
+      end
+
     end
   end
 end
